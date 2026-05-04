4d8dd63aa6d50faed248430e3c9087a7de5e49c5
Prisha0129 <prishanileshjain@gmail.com>
2026-05-04 22:25:17 +0530

    new commit

---
+++ if_condition_statment.py
@@ -0,0 +1,17 @@
+
+def users_input(user1, user2):
+  
+ if user1 == user2:
+    return True
+           
+ else:
+    return False
+user1 = (input())
+user2 = input()
+
+user1 == user1 == "True"
+user2 == user2 == "True"
+
+
+result = users_input(user1,user2)
+print(result)
